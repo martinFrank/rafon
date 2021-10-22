a8930a65066248c5b2e6df441541f2d681e5b6d4
Martin Frank <martin.frank.privat@googlemail.com>
2021-10-22 21:24:59 +0200

    mini steps

--- src/main/java/com/github/martinfrank/data/entity/Player.java
+++ src/main/java/com/github/martinfrank/data/entity/Player.java
@@ -5,15 +5,15 @@ import javax.persistence.OneToOne;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
-@Entity
+//@Entity
 public class Player {
 
-    @NotEmpty
-    private String displayName = "";
-
-    @NotNull
-    @OneToOne
-    private User user;
+//    @NotEmpty
+//    private String displayName = "";
+//
+//    @NotNull
+//    @OneToOne
+//    private User user;
 
 
 }
