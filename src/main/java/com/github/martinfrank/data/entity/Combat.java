6cafed00c38343686c5c1fd0bc1dade539b2742c
Frank Martin <Martin.Frank@finnova.com>
2021-10-28 10:03:57 +0200

    fixing combat-opponent relation

--- src/main/java/com/github/martinfrank/data/entity/Combat.java
+++ src/main/java/com/github/martinfrank/data/entity/Combat.java
@@ -3,8 +3,8 @@ package com.github.martinfrank.data.entity;
 import com.github.martinfrank.data.AbstractEntity;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
-import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
 @Entity
@@ -13,8 +13,9 @@ public class Combat extends AbstractEntity {
     @NotNull
     private String name;
 
-    @NotEmpty
+    @NotNull
     @OneToOne
+    @JoinColumn(name = "opponent_id")
     private Opponent opponent;
 
 
