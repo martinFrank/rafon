2b6ea644b0677b8dbd6eaea7624e9e997cdd612b
Frank Martin <Martin.Frank@finnova.com>
2021-10-29 08:14:27 +0200

    removed setting home on router link target

--- src/main/java/com/github/martinfrank/views/hellome/HelloMeView.java
+++ src/main/java/com/github/martinfrank/views/hellome/HelloMeView.java
@@ -1,6 +1,5 @@
 package com.github.martinfrank.views.hellome;
 
-import com.github.martinfrank.data.entity.MapArea;
 import com.github.martinfrank.data.entity.Player;
 import com.github.martinfrank.data.entity.PlayerItem;
 import com.github.martinfrank.data.entity.User;
@@ -27,16 +26,12 @@ public class HelloMeView extends VerticalLayout {
 
 //    private final TextField inventoryText;
 
-    public static final String HOME_MAP_AREA_NAME = "character";
-
     private final RepositoryService service;
     private final Player currentPlayer;
 
     public HelloMeView(RepositoryService service) {
         this.service = service;
         currentPlayer = getCurrentPlayer(service.getUserRepository(), service.getPlayerRepository());
-        MapArea homeMapArea = service.getMapAreaRepository().findByName(HOME_MAP_AREA_NAME);
-        currentPlayer.setCurrentArea(homeMapArea);
         service.getPlayerRepository().save(currentPlayer);
 
         setMargin(true);
